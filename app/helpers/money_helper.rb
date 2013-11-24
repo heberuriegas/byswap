6f4d7905615ead677f57930b56cbc072ae450e93
Heber Garza <heber.fernando@gmail.com>
2013-11-24 03:35:50 -0300

    Add currencies

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -3,7 +3,7 @@ module MoneyHelper
     if current_currency == 'USD'      
       "1 #{current_currency} ~= #{1.to_money(current_currency).exchange_to(target_currency)} #{target_currency}"
     else
-      "1 #{target_currency} ~= #{1.to_money(target_currency).exchange_to(current_currency)} #{target_currency}"
+      "1 #{target_currency} ~= #{1.to_money(target_currency).exchange_to(current_currency)} #{current_currency}"
     end
   end
 end
